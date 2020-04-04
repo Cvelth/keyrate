718d462b796d80a72c60376341ab32bf1948944e
cvelth <cvelth.mail@gmail.com>
2020-04-04 17:41:59 +0300

    The sources were rewritten with C++. <iostream> is now used for output instead of "stdio.h". std::from_char is now used instead of "strtol". More verbose output and replacement confirmation were added, as well.

--- keyrate.cpp
+++ keyrate.cpp
@@ -1,35 +1,60 @@
 #include <windows.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
 
-bool parseDword(const char* in, DWORD* out) {
-    char* end;
-    long result = strtol(in, &end, 10);
-    bool success = (errno == 0 && end != in);
-    if (success)
-    {
-        *out = result;
-    }
-    return success;
+#include <charconv>
+#include <optional>
+std::optional<DWORD> parse_dword(const char *in) {
+    DWORD out;
+    auto end = std::strchr(in, '\0');
+    auto &&[ptr, ec] = std::from_chars(in, end, out);
+    if (ec == std::errc() && ptr == end)
+        return out;
+    else
+        return std::nullopt;
 }
 
 int main(int argc, char* argv[]) {
     FILTERKEYS keys = { sizeof(FILTERKEYS) };
+    if (!SystemParametersInfo(SPI_GETFILTERKEYS, 0, (LPVOID) &keys, 0)) {
+        std::cerr << "Error: System call failed. Unable to set keyrate.\n";
+        return 1;
+    }
 
-    if (argc == 3 && parseDword(argv[1], &keys.iDelayMSec)
-                  && parseDword(argv[2], &keys.iRepeatMSec)) {
-        printf("Setting keyrate: delay: %d, rate: %d\n", (int) keys.iDelayMSec, (int) keys.iRepeatMSec);
-        keys.dwFlags = FKF_FILTERKEYSON|FKF_AVAILABLE;
-    } else if (argc == 1) {
-        puts ("No parameters given: disabling.");
-    } else {
-        puts ("Usage: keyrate <delay ms> <repeat ms>\nCall with no parameters to disable.");
+    if (argc == 3) {
+        auto new_delay = parse_dword(argv[1]);
+        auto new_repeat = parse_dword(argv[2]);
+        if (!new_delay || !new_repeat) {
+            std::cerr << "Error: Unsupported Parameters.\n"
+                << "  Make sure you suply numbers as <delay ms> and <repeat ms> values.\n";
+            return 2;
+        }
+        std::cout << "Replace current values with\n"
+            << "    Delay: " << keys.iDelayMSec << " -> " << *new_delay << '\n'
+            << "    Rate: " << keys.iRepeatMSec << " -> " << *new_repeat << '\n'
+            << "You sure, you want to replace the values? [Y/n]: ";
+        char answer = '\0';
+        std::cin >> answer;
+        if (answer == 'y' || answer == 'Y') {
+            keys.iDelayMSec = *new_delay;
+            keys.iRepeatMSec = *new_repeat;
+            keys.dwFlags = FKF_FILTERKEYSON | FKF_AVAILABLE;
+            if (!SystemParametersInfo(SPI_SETFILTERKEYS, 0, (LPVOID) &keys, 0)) {
+                std::cerr << "Error: System call failed. Unable to set keyrate.\n";
+                return 1;
+            } else
+                std::cout << "Success.\n";
+        } else
+            std::cout << "Abort.\n";
         return 0;
+    } else if (argc == 1) {
+        std::cout << "Current values are:\n"
+            << "    Delay: " << keys.iDelayMSec << '\n'
+            << "    Rate: " << keys.iRepeatMSec << '\n'
+            << "To change the values, use \"keyrate <delay ms> <repeat ms>\"\n";
+        return 0;
+    } else {
+        std::cout << "Error: Unsupported Parameters.\n"
+            << "  Usage: keyrate <delay ms> <repeat ms>\n";
+        return 2;
     }
-
-    if (!SystemParametersInfo (SPI_SETFILTERKEYS, 0, (LPVOID) &keys, 0)) {
-        fprintf (stderr, "System call failed.\nUnable to set keyrate.");
-    }
-
-    return 0;
 }
