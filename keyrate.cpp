8a24358fc141fcc9323fa9a54c1e778900e4d4c4
cvelth <cvelth.mail@gmail.com>
2020-04-04 14:19:18 +0300

    Code style of the sources was changed.

--- keyrate.cpp
+++ keyrate.cpp
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-BOOL parseDword(const char* in, DWORD* out)
-{
+bool parseDword(const char* in, DWORD* out) {
     char* end;
     long result = strtol(in, &end, 10);
-    BOOL success = (errno == 0 && end != in);
+    bool success = (errno == 0 && end != in);
     if (success)
     {
         *out = result;
@@ -14,30 +13,21 @@ BOOL parseDword(const char* in, DWORD* out)
     return success;
 }
 
-int main(int argc, char* argv[])
-{
-
+int main(int argc, char* argv[]) {
     FILTERKEYS keys = { sizeof(FILTERKEYS) };
 
-    if (argc == 3
-        && parseDword(argv[1], &keys.iDelayMSec)
-        && parseDword(argv[2], &keys.iRepeatMSec))
-    {
+    if (argc == 3 && parseDword(argv[1], &keys.iDelayMSec)
+                  && parseDword(argv[2], &keys.iRepeatMSec)) {
         printf("Setting keyrate: delay: %d, rate: %d\n", (int) keys.iDelayMSec, (int) keys.iRepeatMSec);
         keys.dwFlags = FKF_FILTERKEYSON|FKF_AVAILABLE;
-    }
-    else if (argc == 1)
-    {
+    } else if (argc == 1) {
         puts ("No parameters given: disabling.");
-    }
-    else
-    {
+    } else {
         puts ("Usage: keyrate <delay ms> <repeat ms>\nCall with no parameters to disable.");
         return 0;
     }
 
-    if (!SystemParametersInfo (SPI_SETFILTERKEYS, 0, (LPVOID) &keys, 0))
-    {
+    if (!SystemParametersInfo (SPI_SETFILTERKEYS, 0, (LPVOID) &keys, 0)) {
         fprintf (stderr, "System call failed.\nUnable to set keyrate.");
     }
 
